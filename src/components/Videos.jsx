76549c831fd61ba45a49d6d372398814419880ad
azacdev <azacdev@gmail.com>
2023-04-09 23:45:58 +0100

    update

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,4 +1,6 @@
 import { Stack, Box } from "@mui/material";
+import {VideoCard, ChannelCard} from "./";
+
 const Videos = ({ videos }) => {
 
   return (
@@ -8,9 +10,12 @@ const Videos = ({ videos }) => {
       justifyContent='start' 
       gap={2}
     >
-      {videos.map((item, idx) => {
-        
-      })}
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item}/>}
+          {/* {item.id.channelId && <ChannelCard channelDetail={item}/>} */}
+        </Box>
+      ))}
     </Stack>
   )
 }
