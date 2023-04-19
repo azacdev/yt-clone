7f008db1a35252ea36dfe0e6b77b849f1a8ecdc4
azacdev <azacdev@gmail.com>
2023-04-19 23:13:12 +0100

    update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,6 +7,7 @@ const Feed = () => {
 
   const [selectedCategory, setSelectedCategory] = useState('New')
   const [videos, setVideos] = useState([])
+  
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
     .then(data => setVideos(data.items))
